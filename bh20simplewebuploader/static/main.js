03cbed808805ccdbff639eaa67e8f8b26c7275b3
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-04-25 22:52:54 +0300

    Remove unnecessary JS

--- bh20simplewebuploader/static/main.js
+++ bh20simplewebuploader/static/main.js
@@ -1,37 +1,21 @@
-let r = new XMLHttpRequest();
-let test;
-r.open("GET", scriptRoot + "/api/getAllaccessions", true);
-r.onreadystatechange = function () {
-  if (r.readyState != 4 || r.status != 200) return;
-  test = r.responseText;
-  console.log(JSON.parse(test));
-};
-r.send();
-let uploadForm = document.getElementById('metadata_upload_form')
-let uploadFormSpot = document.getElementById('metadata_upload_form_spot')
-let fillForm = document.getElementById('metadata_fill_form')
-let fillFormSpot = document.getElementById('metadata_fill_form_spot')
+fetch(scriptRoot + "/api/getAllaccessions")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log('test');
+    console.log(data);
+  })
 
-function setUploadMode() {
-  // Make the upload form the one in use
-  uploadFormSpot.appendChild(uploadForm)
-  fillFormSpot.removeChild(fillForm)
-}
-
-function setFillMode() {
-  // Make the fillable form the one in use
-  uploadFormSpot.removeChild(uploadForm)
-  fillFormSpot.appendChild(fillForm)
-}
-
-function setMode() {
-  // Pick mode based on radio
-  if (document.getElementById('metadata_upload').checked) {
-    setUploadMode()
+/**
+ * Show form if checked
+ */
+let fillFormSpot = document.getElementById('metadata_fill_form_spot');
+function displayForm() {
+  if (document.getElementById('metadata_form').checked) {
+    fillFormSpot.classList.remove("invisible");
   } else {
-    setFillMode()
+    fillFormSpot.classList.add("invisible");
+    console.log("visible");
   }
 }
-
-// Start in mode appropriate to selected form item
-setMode()

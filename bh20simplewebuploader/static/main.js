b203bb27172d7abeef60de5bff561de85c7ff11f
Pjotr Prins <pjotr.public01@thebird.nl>
2020-05-23 07:18:24 -0500

    Website: added menu/toolbar

--- bh20simplewebuploader/static/main.js
+++ bh20simplewebuploader/static/main.js
@@ -1,3 +1,18 @@
+/*
+ * Menu and navigation
+ */
+
+/* Toggle between adding and removing the "responsive" class to topnav
+ * when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 function fetchAPI(apiEndPoint) {
   fetch(scriptRoot + apiEndPoint)
     .then(response => {

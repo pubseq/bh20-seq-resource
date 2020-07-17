7565f72b1bf0d70426947a50d1b9a62edc48071d
Pjotr Prins <pjotr.public01@thebird.nl>
2020-07-17 09:45:58 +0100

    Map is back

--- bh20simplewebuploader/static/map.js
+++ bh20simplewebuploader/static/map.js
@@ -1,43 +1,40 @@
-let map = L.map( 'map', {
-  center: [37.0902, -95.7129],  // Default to U.S.A
-  minZoom: 3,
-  zoom: 0
-});
-L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: ['a','b','c']
-}).addTo( map );
 
-let markers = L.markerClusterGroup().addTo(map)
+var map = L.map( 'mapid', {
+    center: [51.505, -0.09],  // Default to U.S.A
+    minZoom: 2,
+    zoom: 0
+});
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo(map);
+
+var markers = L.markerClusterGroup().addTo(mapid)
 
 
 function drawMap(){
+    var mymap = map;
 
-// initialize the map on the "map" div with a given center and zoom
-var mymap = L.map('mapid').setView([51.505, -0.09], 1);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
-
-fetch(scriptRoot + "api/getCountByGPS")
-    .then(response => {
-    console.log(response)
-      return response.json();
-    })
-    .then(data => {
-
-   for (var i=0; i<data.length;i++) {
-   gps=data[i]["GPS"].split(" ")
-    var circle = L.circle([gps[1], gps[0]], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: parseInt(data[i]["count"])  //not working for whatever reason
-        }).addTo(mymap);
-      }
+    fetch(scriptRoot + "api/getCountByGPS")
+        .then(response => {
+            console.log(response)
+            return response.json();
+        })
+        .then(data => {
+            for (var i=0; i<data.length;i++) {
+                gps=data[i]["GPS"].split(" ")
+                var circle = L.circle([gps[1], gps[0]], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: parseInt(data[i]["count"])  //not working for whatever reason
+                }).addTo(mymap);
+            }
 
       });
+    document.getElementById("map_view").classList.remove("invisible");
+    map.invalidateSize();
 }
 
 
@@ -46,9 +43,10 @@ fetch(scriptRoot + "api/getCountByGPS")
  *
 */
 function updateMapMarkers() {
-    markers.clearLayers(); // remove all markers
-    document.getElementById("results").classList.remove("invisible");
+    // markers.clearLayers(); // remove all markers
+    // document.getElementById("results").classList.remove("invisible");
     document.getElementById("loader").classList.add("invisible");
+    /*
     for (let i = 0; i < data.length; i++) {
         let {"count": fastaCount, GPS, LocationLabel: label } = data[i];
         let coordinates = GPS.split(" ");
@@ -58,10 +56,11 @@ function updateMapMarkers() {
             let point = L.point()
             let marker = L.marker([lat, lon]);
             marker.bindPopup("<b>" + label + "</b><br/>" + "FastaCount: " +fastaCount);
-            markers.addLayer(marker)
+            // markers.addLayer(marker)
         }}
+    */
     // Reload the map
     map.invalidateSize();
-    document.getElementById("map_view").classList.add("invisible");
-    document.getElementById("loader").classList.add("invisible");
+    // document.getElementById("map_view").classList.add("invisible");
+    // document.getElementById("loader").classList.add("invisible");
 }

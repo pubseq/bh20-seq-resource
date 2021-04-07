5f768be930a5142e36fc2e943c332341ae471c62
Pjotr Prins <pjotr.public01@thebird.nl>
2021-04-07 14:24:13 +0200

    http -> https image

--- bh20simplewebuploader/static/map.js
+++ bh20simplewebuploader/static/map.js
@@ -11,7 +11,7 @@ var map = L.map( 'mapid', {
     zoom: 0
 });
 
-L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="http://covid19.genenetwork.org/">COVID-19 PubSeq</a>',
     subdomains: ['a','b','c']
 }).addTo(map);

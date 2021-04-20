13dabbff87a5e8e3d104c19b1df97bb9bea3f475
Pjotr Prins <pjotr.public01@thebird.nl>
2021-04-20 12:16:12 +0200

    Locations

---
+++ workflows/pubseq/normalize-geo.rb
@@ -0,0 +1,7 @@
+#! /usr/bin/env ruby
+#
+# This script takes a metadata file and transforms the geo/location
+# fields into a normalized wikidata URI. This is described in
+# https://github.com/pubseq/bh20-seq-resource/blob/master/doc/blog/covid19-pubseq-location-data.org
+#
+#

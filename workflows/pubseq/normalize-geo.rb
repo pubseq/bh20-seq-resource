938d8230c0587ef525d96b1080636407f1f6e61d
Pjotr Prins <pjotr.public01@thebird.nl>
2021-04-21 12:08:05 +0200

    Normalize-geo

--- workflows/pubseq/normalize-geo.rb
+++ workflows/pubseq/normalize-geo.rb
@@ -1,7 +1,66 @@
 #! /usr/bin/env ruby
 #
+# PubSeq normalize geo info using wikidata URIs. Based on earlier
+# PubSeq scripts (written in Python)
+#
+# Author:: Pjotr Prins
+# License:: MIT
+#
+# Copyright (C) 2021 Pjotr Prins <pjotr.prins@thebird.nl>
+#
 # This script takes a metadata file and transforms the geo/location
 # fields into a normalized wikidata URI. This is described in
 # https://github.com/pubseq/bh20-seq-resource/blob/master/doc/blog/covid19-pubseq-location-data.org
 #
-#
+
+TOOL=File.basename($0)
+
+GEMPATH = File.dirname(__FILE__) + '/../../lib/ruby'
+$: << File.join(GEMPATH,'lib/ruby/pubseq')
+
+VERSION_FILENAME=File.join(GEMPATH,'VERSION')
+VERSION = File.new(VERSION_FILENAME).read.chomp
+
+require 'optparse'
+require 'fileutils'
+require 'json'
+
+options = { show_help: false, source: 'https://github.com/pubseq', version: VERSION+' (Pjotr Prins)', date: Time.now.to_s }
+
+opts = OptionParser.new do |o|
+  o.banner = "Usage: #{TOOL} [options] path"
+  o.on("-q", "--quiet", "Run quietly") do |q|
+    # Bio::Log::CLI.trace('error')
+    options[:quiet] = true
+  end
+
+  o.on("-v", "--verbose", "Run verbosely") do |v|
+    options[:verbose] = true
+  end
+
+  o.on("--debug", "Show debug messages and keep intermediate output") do |v|
+    # Bio::Log::CLI.trace('debug')
+    options[:debug] = true
+  end
+
+  o.separator ""
+  o.on_tail('-h', '--help', 'display this help and exit') do
+    options[:show_help] = true
+  end
+end
+
+opts.parse!(ARGV)
+
+NORMALIZE_GEO_BANNER = "#{TOOL} #{VERSION} (Ruby #{RUBY_VERSION}) by Pjotr Prins 2021\n"
+$stderr.print NORMALIZE_GEO_BANNER if !options[:quiet]
+
+if options[:show_help]
+  print opts
+  exit 1
+end
+
+if RUBY_VERSION =~ /^[12]/
+  $stderr.print "WARNING: #{TOOL} may not run properly on Ruby <3.x\n"
+end
+
+$stderr.print "Options: ",options,"\n" if !options[:quiet]
